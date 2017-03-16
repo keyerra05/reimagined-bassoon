824976d5dfd76b342567100fb891634e4861fa1d
keyerra05 <keyerra.king@gmail.com>
2017-03-16 23:51:12 +0000

    login authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create 
+    delete 'logout' => :destroy 
+    end
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
-  get 'store/index'
-
- resources :users
+  root 'store#index', as: 'store_index'
  resources :products
  #root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root "store#index"
 end
 
